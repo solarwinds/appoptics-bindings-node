ce2e41aed0d2c010c537ed3446dc0f6370167583
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-11-12 16:51:59 -0800

    try to use bmac/test-hello-world

--- docker/build-and-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-cd /root || exit 1
-git clone --depth=1 https://github.com/appoptics/appoptics-bindings-node aob
-cd aob || exit 1
-npm install -g mocha
-npm install --production --unsafe-perm
