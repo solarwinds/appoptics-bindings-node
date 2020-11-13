c16365d04e8af8e93d4a39ab2b5ff5928bbe179b
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-11-13 13:51:33 -0800

    try some outputs

--- .github/actions/alpine/build-and-test-bindings.sh
+++ .github/actions/alpine/build-and-test-bindings.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
+succeeded=
+failed=
+
 branch="$1"
 # shellcheck disable=SC2034 # used by appoptics-bindings in tests
 export AO_TOKEN_PROD="$2"
@@ -10,19 +13,34 @@ cd aob || exit 1
 
 # make sure production install works
 if ! npm install --production --unsafe-perm; then
+  echo "::set-output name=install-prod::fail"
   error=true
+else
+  echo "::set-output name=install-prod::pass"
 fi
 
 rm -rf node_modules
 
 # install so testing works
 if ! npm install --unsafe-perm; then
+  echo "::set-output name=install-dev::fail"
   error=true
+else
+  echo "::set-output name=install-dev::pass"
 fi
 
 # test
 npm install -g mocha || error=true
-npm test || error=true
+
+if ! npm test; then
+  echo "::set-output name=install-dev::fail"
+  error=true
+else
+  echo "::set-output name=install-dev::pass"
+fi
+
+echo "::set-output name=good-tests::nyi"
+echo "::set-output name=bad-tests::nyi"
 
 if [ -n "$error" ]; then
   exit 1
