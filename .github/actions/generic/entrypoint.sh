013351d3b784c62d45203732e6cb1a1b01ed8ab3
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-11-21 14:51:19 -0800

    test

--- .github/actions/generic/entrypoint.sh
+++ .github/actions/generic/entrypoint.sh
@@ -8,6 +8,7 @@ echo "::set-output name=all-args::$*"
 
 cd /docker-action || exit 1
 echo "creating docker image with alpine version: $OS_VERSION"
+echo "make a change"
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
