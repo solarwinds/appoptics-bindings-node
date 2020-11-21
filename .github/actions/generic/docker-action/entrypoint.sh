6c882a1685443c0182dc7f6530bf9613130145b2
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-11-21 14:44:53 -0800

    get whole os-release

--- .github/actions/generic/docker-action/entrypoint.sh
+++ .github/actions/generic/docker-action/entrypoint.sh
@@ -2,7 +2,9 @@
 branch=$1
 echo "::set-output name=all-args-2::$*"
 
-version=$(cat /etc/alpine-release)
+# make os-release one line and get rid of the garbage that confuses github actions. the
+# parens and ANSI removal are for centos but no harm here.
+details=$(tr -d '()' < /etc/os-release | tr '\n' ',' | sed 's/ANSI_COLOR="0;31",//')
+echo "::set-output name=os-details::$details"
 
-echo "::set-output name=version::${version}"
 echo "::set-output name=branch-to-test::${branch}"
